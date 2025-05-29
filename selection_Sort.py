559e49d7f5fb635ad30d08c9309936af0981592d
cobra <rickyhackermsr@gmail.com>
2025-05-29 13:48:53 +0530

    selection sort

--- selection_Sort.py
+++ selection_Sort.py
@@ -1,10 +1,7 @@
-a=[3,2,1,5,6]
-temp = a[0]
-al=len(a)
-for i in range(al-1):
-    for j in range (i+1,al):
-        if a[i]>a[j]:
-             temp=a[j]
-             a[j]=a[i]
-             a[i]=temp
+a = [3, 2, 1, 5, 6]
+al = len(a)
+for i in range(al - 1):
+    for j in range(i + 1, al):
+        if a[i] > a[j]:
+            a[i], a[j] = a[j], a[i]
 print(a)
